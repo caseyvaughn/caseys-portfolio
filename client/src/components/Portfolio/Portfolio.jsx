c6b315bd42e39a768f59b4177348a1dc52c10a07
Casey Vaughn <caseyvaughn@caseys-mbp-2.myfiosgateway.com>
2022-03-08 10:36:42 -0500

    added github icon links to project cards

--- client/src/components/Portfolio/Portfolio.jsx
+++ client/src/components/Portfolio/Portfolio.jsx
@@ -1,6 +1,6 @@
 import { Card, Row, Col, Container} from 'react-bootstrap'
-// import { Link } from 'react-router-dom'
 import "./Portfolio.css"
+import github from "../../assets/icons/github.png"
 
 export default function Portfolio() {
   
@@ -8,22 +8,22 @@ export default function Portfolio() {
     <div>
       <h1>Recent Work</h1>
       <Container>
-        {/* <Row>
-          <Col> */}
             <Row className='row-cols-1 row-cols-lg-3'>
               <Col>
                 <Card className="portfolio-card card">
-                {/* // <Card style={{ borderRadius: "25px",backgroundColor: "#d2d0e7"}}> */}
+
                 {/* <Card.Img variant="top" src="holder.js/100px160" /> */}
                 <Card.Body>
                   <Card.Title>Wordle With Friends</Card.Title>
                   <Card.Text>
                     A Wordle game clone that allows users to create custom Wordle games and to solve Wordles created by other users.
-                      https://wordle-with-friendz.netlify.app/
-                    </Card.Text>
-                    <a className = "extern-link" href="https://wordle-with-friendz.netlify.app/">Deployed Site</a>
-                </Card.Body>
-                  </Card>
+                  
+                  </Card.Text>
+                      <a href="https://github.com/caseyvaughn/wordle-with-friends"><img className = "icon" src={github} alt="github logo"/></a>
+                <br />
+                <a className="extern-link" href="https://wordle-with-friendz.netlify.app/">Deployed Site</a>
+                  </Card.Body>
+                </Card>
               </Col>
               <Col>
               <Card className="portfolio-card">
@@ -34,7 +34,10 @@ export default function Portfolio() {
                       A reading tracking website that allows users to log their reading habits and review books.
                       
                     </Card.Text>
-                    <a className = "extern-link" href="https://caseys-great-reads.netlify.app/">Deployed Site</a>
+                
+                <a href="https://github.com/caseyvaughn/greatReads"><img className="icon" src={github} alt="github logo" /></a>
+                <br/>
+                <a className="extern-link" href="https://caseys-great-reads.netlify.app/">Deployed Site</a>
                 </Card.Body>
                   </Card>
               </Col>
@@ -45,18 +48,15 @@ export default function Portfolio() {
                   <Card.Title>Tarot Reader</Card.Title>
                   <Card.Text>
                       Users to draw a virtual Tarot reading and to search for a specific card using data from the Tarot API.
-                      https://caseyvaughn.github.io/Tarot-Reader/
-                    </Card.Text>
+                </Card.Text>
+                <a href="https://github.com/caseyvaughn/Tarot-Reader"><img className="icon" src={github} alt="github logo" /></a>
+                <br/>
                     <a className = "extern-link" href="https://caseyvaughn.github.io/Tarot-Reader/">Deployed Site</a>
                 </Card.Body>
                   </Card>
               </Col>
               </Row>
-          {/* </Col>
-        </Row> */}
       </Container>
-      
-      
     </div>
   )
 }
