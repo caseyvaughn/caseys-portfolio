cf54ff11184d55966dbdfb2217eedca5475b12c2
Casey Vaughn <caseyvaughn@caseys-mbp-2.myfiosgateway.com>
2022-03-09 17:31:34 -0500

    removed comment that was throwing errors on deployment from landing page

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./LandingPage.scss"
-{/* //https://stackoverflow.com/questions/50209799/when-can-an-empty-array-be-iterated-over#:~:text=An%20array%20of%20undefined%20s%20can%20be%20iterated%20over%20%2D%20here,not%20be%20assigned%20to%20anything.&text=Note%20that%20you%20shouldn't,over%20each%20item%20and%20console. */}
+
 
 export default function LandingPage() {
   return (
